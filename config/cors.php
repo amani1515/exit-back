a9d160f39a4ea57960095443010d73ee38c47f4a
Amanuel <amanuelmhret16@gmail.com>
2026-05-28 01:02:07 +0300

    updated

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://exam.aviator250.com', 'https://aradabingo.bond'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
